a07f4b534de5624de70ed5abc4a35e99946d7b5b
Владислав <vladislav.puzzyrev@gmail.com>
2020-04-10 01:46:40 +0500

    Updates

---
+++ precache-manifest.02844e60c8f62d83de9908c73dde2657.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba6837ace130c0065e1a7f0a4102a034",
+    "url": "/vk_spamer_online/index.html"
+  },
+  {
+    "revision": "1104ad08b59c7e4002e9",
+    "url": "/vk_spamer_online/static/css/main.06d2b413.chunk.css"
+  },
+  {
+    "revision": "730976ab95f59c6a0860",
+    "url": "/vk_spamer_online/static/js/2.8a93e490.chunk.js"
+  },
+  {
+    "revision": "885c0e205ef5e79f4aef448a818ba6a1",
+    "url": "/vk_spamer_online/static/js/2.8a93e490.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1104ad08b59c7e4002e9",
+    "url": "/vk_spamer_online/static/js/main.74a73c18.chunk.js"
+  },
+  {
+    "revision": "74abcd2cf4711a24b35f",
+    "url": "/vk_spamer_online/static/js/runtime-main.1c9b5966.js"
+  },
+  {
+    "revision": "3e8eb14fc6f35ccb54c3d2ec6459e67b",
+    "url": "/vk_spamer_online/static/media/comments.3e8eb14f.png"
+  },
+  {
+    "revision": "2be4341cae06b28b1071b5aee8492409",
+    "url": "/vk_spamer_online/static/media/logo.2be4341c.png"
+  },
+  {
+    "revision": "25b700def2d264e92ee91f0c8ab207f8",
+    "url": "/vk_spamer_online/static/media/pm.25b700de.png"
+  },
+  {
+    "revision": "7e942421b15a35ad8568e06c0cacc428",
+    "url": "/vk_spamer_online/static/media/talks.7e942421.png"
+  },
+  {
+    "revision": "8711f0bb5776c92032e9d86226bfa555",
+    "url": "/vk_spamer_online/static/media/users-walls.8711f0bb.png"
+  }
+]);
